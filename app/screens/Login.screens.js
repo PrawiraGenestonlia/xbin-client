dde68e29a663d8bef213fc187f3aaa78591a9866
PrawiraGenestonlia <prawira.96@gmail.com>
2019-10-06 21:32:38 +0800

    Fast Commit

--- app/screens/Login.screens.js
+++ app/screens/Login.screens.js
@@ -4,7 +4,7 @@ import { useTheme } from 'react-navigation';
 import { COLOR } from '../constants';
 import GlobalStyle from './global.stylesheets';
 
-//use apple and google sdk
+//use apple and google sdks
 export default function Login(props) {
   let theme = useTheme();
   const { navigation } = props;
